284e18c2530fc3e12b08524ca135e8f3fe582ce1
Rifky Ekayama <rifqykiw@gmail.com>
2016-10-11 08:10:17 +0700

    add dependency whatsapp

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         DaveJamesMiller\Breadcrumbs\ServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
+        Xaamin\Whatsapi\WhatsapiServiceProvider::class,
 
         //
 
@@ -233,6 +234,8 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Breadcrumbs' => DaveJamesMiller\Breadcrumbs\Facade::class,
+        'Whatsapi' => Xaamin\Whatsapi\Facades\Laravel\Whatsapi::class,
+        'WhatsapiTool' => Xaamin\Whatsapi\Facades\Laravel\Registration::class,
 
     ],
 
